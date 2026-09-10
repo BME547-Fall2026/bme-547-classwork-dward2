da5938b4cc5f487b45a30d481b23b3e23d03344b
David Ward <daw74@duke.edu>
2026-09-10 09:01:00 -0400

    Demonstration of functional testing

---
+++ test_weight_entry.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.mark.parametrize("weight_input, expected", [
+    ("105 lb", 48),
+    ("22 kg", 22),
+    ("105.3 lb", 48),
+    ("22 KG", 22),
+    ("22 Pounds", 10),
+    ("1000 g", 1),
+    ("", 0),
+    ("-22 lb", -10),
+    # ("22", 
+    ("22 lbs", 10),
+    ])
+def test_parse_weight_input(weight_input, expected):
+    from weight_entry import parse_weight_input
+    answer = parse_weight_input(weight_input)
+    assert answer == expected
