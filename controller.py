3cfe34f2f2a6ae63e79a0a4f807a91953f5ce425
David Ward <daw74@duke.edu>
2026-09-21 21:57:09 -0400

    PEP-8 fixes for GUI file

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-db =[]
+db = []
 
 
 def new_patient(patient_name,
@@ -10,7 +10,7 @@ def new_patient(patient_name,
     db.append(patient)
     save_database()
     return "Patient added to database"
-    
+
 
 def save_database():
     out_file = open("patient_monitor_db.txt",
@@ -22,7 +22,7 @@ def save_database():
             patient["dob"])
         out_file.write(out_string)
     out_file.close()
-    
+
     with open("patient_monitor_db.txt", "w") as out_file:
         for patient in db:
             out_string = "{},{},{}\n".format(
@@ -31,13 +31,12 @@ def save_database():
                 patient["dob"])
             out_file.write(out_string)
     print("done")
-    
-    
+
+
 def read_database():
     with open("patient_monitor_db.txt", "r") as in_file:
         all_lines = in_file.readlines()
     print(all_lines)
-    
-    
+
+
 read_database()
-    
--- gui.py
+++ gui.py
@@ -4,9 +4,9 @@ import controller
 text_font = ("Arial", 24)
 status_font = ("Arial", 14)
 
-    
+
 def main_window():
-    
+
     def submit_btn_cmd():
         # Get needed data from the GUI
         patient_name = name_value.get()
@@ -18,7 +18,7 @@ def main_window():
                                         patient_dob)
         # Update the GUI with answer as needed
         status_label.configure(text=answer)
-    
+
     root = tk.Tk()
     root.title("Patient Monitor")
     root.geometry("800x500")
@@ -27,17 +27,17 @@ def main_window():
                           borderwidth=2,
                           relief="groove")
     data_frame.grid(column=0, row=0, sticky=tk.N)
-    
-    title_label = tk.Label(data_frame, 
+
+    title_label = tk.Label(data_frame,
                            text="Data Frame Placeholder",
                            font=text_font)
     title_label.grid(column=0, row=0)
-    
+
     patient_frame = tk.Frame(root,
                              borderwidth=2,
                              relief="groove")
     patient_frame.grid(column=1, row=0)
-    
+
     name_label = tk.Label(patient_frame, text="Name:",
                           font=text_font)
     name_label.grid(column=0, row=0, sticky=tk.E)
@@ -53,7 +53,7 @@ def main_window():
     mrn_entry = tk.Entry(patient_frame, font=text_font,
                          textvariable=mrn_value)
     mrn_entry.grid(column=1, row=1)
-    
+
     dob_label = tk.Label(patient_frame, text="DOB:",
                          font=text_font)
     dob_label.grid(column=0, row=2, sticky=tk.E)
@@ -61,12 +61,12 @@ def main_window():
     dob_entry = tk.Entry(patient_frame, font=text_font,
                          textvariable=dob_value)
     dob_entry.grid(column=1, row=2)
-    
+
     submit_btn = tk.Button(patient_frame, text="Submit",
                            font=text_font,
                            command=submit_btn_cmd)
     submit_btn.grid(column=0, row=3)
-    
+
     status_label = tk.Label(root, font=status_font)
     status_label.grid(column=0, row=1, sticky=tk.W)
 
